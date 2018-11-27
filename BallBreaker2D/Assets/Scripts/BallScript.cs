a39311c1a3e66f48c2d8e1f291c42d237cf355c6
RobertWais <wais.robert@uwlax.edu>
2018-11-26 23:36:32 -0600

    Added more levels

--- BallBreaker2D/Assets/Scripts/BallScript.cs
+++ BallBreaker2D/Assets/Scripts/BallScript.cs
@@ -12,11 +12,13 @@ public class BallScript : MonoBehaviour {
     [SerializeField] float randomMovement = 0.2f;
     bool hasStarted = false;
 
+    Rigidbody2D myRigidBody;
 
     Vector2 paddleBallGap;
 	// Use this for initialization
 	void Start () {
         paddleBallGap = transform.position - paddle1.transform.position;
+        myRigidBody = GetComponent<Rigidbody2D>().
 	}
 	
 	// Update is called once per frame
@@ -31,7 +33,7 @@ public class BallScript : MonoBehaviour {
     {
         if(Input.GetMouseButtonDown(0))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush,yPush);
+            myRigidBody.velocity = new Vector2(xPush,yPush);
             hasStarted = true;
         }
     }
@@ -39,6 +41,12 @@ public class BallScript : MonoBehaviour {
     private void LockBall(){
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePos + paddleBallGap;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Vector2 velocity = new Vector2(UnityEngine.Random.Range(0f, randomMovement), UnityEngine.Random.Range(0f, randomMovement));
+        myRigidBody.velocity += velocity;
     }
 
 }
